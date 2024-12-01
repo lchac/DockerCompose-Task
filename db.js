b3b18125a3e74bc2365b670a02f4efa0320c7d6a
Luis <32205749+lchac@users.noreply.github.com>
2024-12-01 09:39:20 -0500

    Add MongoDB pod

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connect() {
-  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test"; //
+  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test";
   try {
     await mongoose.connect(uri);
   } catch (err) {
